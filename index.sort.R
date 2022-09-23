771c5681093ddb35373c81eb1a2c2e2f8187f4cf
hshintaku <hirofumi.shintaku@riken.jp>
2022-09-23 17:30:43 +0900

    update DTD

--- index.sort.R
+++ index.sort.R
@@ -3,3 +3,5 @@ index <- Read10X(data.dir = datadir)
 index.dtd <- Read10X(data.dir = file.path(datadir,"CITE-seq"))
 
 index.dtd.subset <- index.dtd[,str_sub(colnames(index.dtd),9,21) %in% str_sub(colnames(index),9,21)]
+
+
