1a1eac0eac414f8ba96675755d38e62225b8a86a
A-Shiomi <akifumi.shiomi@riken.jp>
2022-12-02 21:56:49 +0900

    20221202

--- cancer6_Seurat.clustering.R
+++ cancer6_Seurat.clustering.R
@@ -1,65 +1,39 @@
 cancer <- FindVariableFeatures(cancer, selection.method = "vst", nfeatures = 300)
 cancer <- NormalizeData(cancer,assay="DTD",normalization.method = "CLR",scale.factor = 1e2)
 cancer[["percent.mt"]] <- PercentageFeatureSet(cancer, pattern = "^MT-")
-#cancer <- subset(cancer,subset=percent.mt<5)
+cancer <- subset(cancer,subset=percent.mt<10)
 #
 # PCA and visualize
+#
 all.genes <- rownames(cancer)
 cancer <- ScaleData(cancer, features = all.genes)
 cancer <- RunPCA(cancer, npcs=50, features = VariableFeatures(object = cancer))
-
 DimPlot(cancer, reduction = "pca",group.by = "run")+
-  DimPlot(cancer,reduction="pca",group.by = "celltype")+
   FeaturePlot(cancer,features = "dtd_FLD004")
-
 cancer <- JackStraw(cancer, num.replicate = 100)
 cancer <- ScoreJackStraw(cancer, dims = 1:20)
 JackStrawPlot(cancer, dims = 1:20)
 ElbowPlot(cancer)
-
-cancer <- FindNeighbors(cancer, dims = 1:10)
-cancer <- FindClusters(cancer, resolution = 0.1)
-
-
-# Retreiving the results of the preprocessing from the Seurat object
-cluster = as.numeric(Idents(cancer))
-cancer <- RunUMAP(cancer, dims = 1:10)
-DimPlot(cancer, reduction = "pca",group.by = "celltype")+
-  DimPlot(subset(cancer,subset=run=="forth"),reduction="pca")+
-  FeaturePlot(cancer,features = c("dtd_FLD500"), reduction = "umap")
-
 #
-# subset third run
-cancer6<-subset(cancer,subset=run=="sixth")
-cancer6 <- FindVariableFeatures(cancer6, selection.method = "vst", nfeatures = 300)
-all.genes <- rownames(cancer6)
-cancer6 <- ScaleData(cancer6, features = all.genes)
-cancer6 <- RunPCA(cancer6, npcs=50, features = VariableFeatures(object = cancer6))
+# subset fifth run
+#
+cancer <- RunUMAP(cancer, dims = 1:10)
+cancer <- RunTSNE(cancer, npcs=50, features = VariableFeatures(object = cancer))
+cancer <- FindNeighbors(cancer, dims = 1:10)
+cancer <- FindClusters(cancer, resolution = 0.04)
+cluster = as.numeric(Idents(cancer))
 
-cancer6 <- FindNeighbors(cancer6, dims = 1:10)
-cancer6 <- FindClusters(cancer6, resolution = 0.04)
-DimPlot(cancer6, reduction = "pca")+
-  FeaturePlot(cancer6,features = "dtd_FLD004", reduction = "pca")
-DimPlot(cancer6,reduction = "pca")+
-  FeaturePlot(cancer6,
-              features = c("CCDC190","CAVIN3","CD163L1","PLAU","FOSL1"),reduction = "pca",
-              max.cutoff = 4)
-DimPlot(cancer,reduction = "pca",group.by = "celltype")+
-  FeaturePlot(cancer,
-              features = c("CCDC190","CAVIN3","CD163L1","PLAU","FOSL1"),reduction = "pca",
-              max.cutoff = 4)
+DimPlot(cancer,reduction = "umap")+
+  FeaturePlot(cancer, features = c("DUSP1","CAVIN3","CDKN1A","CYBA","S100A2", "nCount_RNA"),reduction = "umap")
+cancer <- RenameIdents(cancer, `2` = "MCF10A", `1` = "MCF7", `0` = "unknown")
 
-
-cancer6 <- RenameIdents(cancer6, `2` = "MCF10A", `1` = "MCF7", `0` = "MCF7")
-
-#cancer1 <- RenameIdents(cancer1, `0` = "MCF7_NEP", `1` = "PC3_NEP")
-cancer_mcf10<-subset(cancer6,idents = "MCF10A")
+cancer_mcf10<-subset(cancer,idents = "MCF10A")
 cancer_mcf10[["celltype"]]<-"MCF10A"
 cancer_mcf10[["NEP"]]<-"40V"
 cancer_mcf10[["hashtag"]]<-"MCF10C"
 cancer_mcf10[["condition"]]<-"CytoD"
 
-cancer_mcf7<-subset(cancer6,idents="MCF7")
+cancer_mcf7<-subset(cancer,idents="MCF7")
 cancer_mcf7[["celltype"]]<-"MCF7"
 cancer_mcf7[["condition"]]<-"normal"
 cancer_mcf7[["NEP"]]<-"40V"
@@ -68,6 +42,7 @@ cancer_mcf7[["hashtag"]]<-"MC7E37"
 cancer6_merge<- merge(cancer_mcf10,y=cancer_mcf7)
 
 cancer_merge123456 <-merge(cancer_merge12345,y=cancer6_merge)
+cancer_merge123456 <- subset(cancer_merge123456,subset=nFeature_RNA>2500)
 cancer_merge123456 <- FindVariableFeatures(cancer_merge123456, selection.method = "vst", nfeatures = 300)
 all.genes <- rownames(cancer_merge123456)
 cancer_merge123456 <- ScaleData(cancer_merge123456, features = all.genes)
@@ -77,42 +52,10 @@ cancer_merge123456 <- RunUMAP(cancer_merge123456, dims = 1:10)
 cancer_merge123456 <- RunTSNE(cancer_merge123456,dims=1:10)
 
   FeaturePlot(cancer_merge123456,
-              features = c("CYBA","S100A2","CCDC190","CAVIN3","CD163L1","PLAU","FOSL1","percent.mt"),
+              features = c("CYBA","S100A2","CAVIN3","FOSL1","CDKN1A","DUSP1"),
               reduction = "tsne",
               max.cutoff = 10)+
     DimPlot(cancer_merge123456,reduction = "tsne",group.by = "celltype")+
     DimPlot(cancer_merge123456,reduction = "tsne",group.by = "NEP")
-  
-  
-  FeaturePlot(subset(cancer_merge123456,subset=percent.mt<10),
-              features = c("CYBA","S100A2","CCDC190","CAVIN3","PLAU","FOSL1"),
-              reduction = "tsne",
-              max.cutoff = 10)
 
-  p1<-FeaturePlot(subset(subset(cancer_merge123456,subset=NEP== "0V"),subset=condition=="normal"),
-                features = c("percent.mt"),
-                reduction = "tsne",
-                max.cutoff = 10)
-    p2<-FeaturePlot(subset(subset(cancer_merge123456,subset=NEP== "0V",invert=TRUE),subset=condition=="normal"),
-                features = c("percent.mt"),
-                reduction = "tsne",
-                max.cutoff = 10)
-    
-    p3<-DimPlot(subset(subset(cancer_merge123456,subset=percent.mt<10),subset=condition=="normal"),reduction = "tsne",group.by = "celltype")
-    p4<-DimPlot(subset(cancer_merge123456,subset=condition=="normal"),reduction = "tsne",group.by = "NEP")
-    p5<-DimPlot(subset(subset(cancer_merge123456,subset=percent.mt<10),subset=condition=="normal"),reduction = "tsne",group.by = "NEP")
-    p6<-DimPlot(subset(subset(cancer_merge123456,subset=percent.mt<5),subset=condition=="normal"),reduction = "tsne",group.by = "NEP")
-  gridExtra::grid.arrange(p1,p2,p3,p4,p5,p6,nrow=3)
-  
-      FeaturePlot(subset(subset(cancer_merge123456,subset=percent.mt<5),subset=condition=="normal"),features ="nCount_RNA",
-              reduction = "tsne",max.cutoff = 10000)
-  
-DimPlot(subset(cancer_merge123456,subset=percent.mt<5),reduction = "umap",group.by = "celltype")+
-  FeaturePlot(subset(cancer_merge123456,subset=percent.mt<5),
-              features = c("CYBA","S100A2","CCDC190","CAVIN3","CD163L1","PLAU","FOSL1"),
-              reduction = "umap",
-              max.cutoff = 4)
-
-rm(cancer,cancer_mcf10,cancer_mcf7,cancer_mda,cancer_merge,cancer_merge12345,cancer_pc3,cancer_pc3_CTL,cancer_pc3_NEP)
-rm(cancer1,cancer2,cancer2_mcf7,cancer2_pc3,cancer3_merge,cancer3,cancer6,cancer6_merge)
-rm(p1,p2,p3,p4,p5,p6)
+rm(cancer,cancer_mcf10,cancer_mcf7,cancer6_merge, cancer6, cancer_merge1234, cancer_merge12345)
