3d811a85d5ae2e79c5777b29a90c8527d132481d
A-Shiomi <akifumi.shiomi@riken.jp>
2022-12-02 21:55:57 +0900

    20221202

--- cancer2_Seurat.clustering.R
+++ cancer2_Seurat.clustering.R
@@ -1,46 +1,37 @@
 cancer <- NormalizeData(cancer, normalization.method = "LogNormalize", scale.factor = 1e5)
 cancer[["percent.mt"]] <- PercentageFeatureSet(cancer, pattern = "^MT-")
-#cancer <- subset(cancer, subset= percent.mt<5)
-#
+cancer <- subset(cancer, subset= percent.mt<10)
 #
 # merge all the tig data
 #
 cancer <- FindVariableFeatures(cancer, selection.method = "vst", nfeatures = 300)
-# Identify the 10 most highly variable genes
-#top10 <- head(VariableFeatures(cancer), 40)
-# plot variable features with labels
-#plot1 <- VariableFeaturePlot(cancer)
-#plot1 <- LabelPoints(plot = cancer, points = top10)
-#plot1
-# normalize the dtd data with "RC" option.
 cancer <- NormalizeData(cancer,assay="DTD",normalization.method = "CLR",scale.factor = 1e6)
 #
 # PCA and visualize
+#
 all.genes <- rownames(cancer)
 cancer <- ScaleData(cancer, features = all.genes)
 cancer <- RunPCA(cancer, npcs=20, features = VariableFeatures(object = cancer))
 DimPlot(cancer, reduction = "pca",group.by = "run")
-
 cancer <- JackStraw(cancer, num.replicate = 100)
 cancer <- ScoreJackStraw(cancer, dims = 1:20)
 JackStrawPlot(cancer, dims = 1:20)
 ElbowPlot(cancer)
-
 cancer <- FindNeighbors(cancer, dims = 1:10)
 cancer <- FindClusters(cancer, resolution = 0.02)
-
-
+#
 # Retreiving the results of the preprocessing from the Seurat object
+#
 cluster = as.numeric(Idents(cancer))
 cancer <- RunUMAP(cancer, dims = 1:10)
 DimPlot(cancer,reduction="pca")+
   FeaturePlot(cancer,features = c("dtd_P3CE36","dtd_MC7E37","dtd_FLD004"),
               reduction = "pca",slot="counts",
               max.cutoff = 100)
-# DLGAP5: cancer-1-20
-# ITGA8: cancer-1-50
-
-cancer <- RenameIdents(cancer, `1` = "PC3_NEP", `0` = "MCF7_CTL")
+#
+#identification of each cell type
+#
+cancer <- RenameIdents(cancer, `0` = "PC3_NEP", `1` = "PC3_NEP", `2` = "MCF7_CTL")
 #FindAllMarkers(cancer)
 cancer2_mcf7<-subset(cancer,idents = "MCF7_CTL")
 cancer2_mcf7[["celltype"]]<-"MCF7"
