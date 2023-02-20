50813a5aa5c9a21652abba4cb0bbc744e47eb03f
hshintaku <hirofumi.shintaku@riken.jp>
2023-02-20 14:30:55 +0900

    update for six batches

--- cancer2_Seurat.clustering.R
+++ cancer2_Seurat.clustering.R
@@ -1,6 +1,6 @@
 cancer <- NormalizeData(cancer, normalization.method = "LogNormalize", scale.factor = 1e5)
 cancer[["percent.mt"]] <- PercentageFeatureSet(cancer, pattern = "^MT-")
-#cancer <- subset(cancer, subset= percent.mt<5)
+cancer <- subset(cancer, subset= percent.mt<10 & nCount_RNA>=2000)
 #
 #
 # merge all the tig data
@@ -31,16 +31,16 @@ cancer <- FindClusters(cancer, resolution = 0.02)
 
 
 # Retreiving the results of the preprocessing from the Seurat object
+cancer<-NormalizeData(cancer,assay = "DTD",normalization.method = "CLR")
 cluster = as.numeric(Idents(cancer))
 cancer <- RunUMAP(cancer, dims = 1:10)
 DimPlot(cancer,reduction="pca")+
-  FeaturePlot(cancer,features = c("dtd_P3CE36","dtd_MC7E37","dtd_FLD004"),
-              reduction = "pca",slot="counts",
+  FeaturePlot(cancer,features = c("dtd_P3CE36","CYBA","S100A2","dtd_FLD004"),
+              reduction = "pca",
               max.cutoff = 100)
-# DLGAP5: cancer-1-20
-# ITGA8: cancer-1-50
 
-cancer <- RenameIdents(cancer, `1` = "PC3_NEP", `0` = "MCF7_CTL")
+
+cancer <- RenameIdents(cancer, `0` = "PC3_NEP", `1` = "MCF7_CTL")
 #FindAllMarkers(cancer)
 cancer2_mcf7<-subset(cancer,idents = "MCF7_CTL")
 cancer2_mcf7[["celltype"]]<-"MCF7"
@@ -69,3 +69,4 @@ DimPlot(cancer2, reduction = "pca",group.by = "celltype")
     VlnPlot(subset(cancer2, subset= percent.mt<5),features = "FOSL1", group.by = "celltype")
 FeatureScatter(cancer2,feature1 = "dtd_P3CE36",feature2 = "dtd_FLD004",slot = "counts")
 VlnPlot(cancer2,features =c("percent.mt","nCount_RNA"), group.by = "celltype")
+
