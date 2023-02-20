8fbac23db2e0fbc5be427659f0d9a4988b35eb72
A-Shiomi <akifumi.shiomi@riken.jp>
2023-02-20 14:08:26 +0900

    230220

--- cancer2_Seurat.clustering.R
+++ cancer2_Seurat.clustering.R
@@ -1,62 +1,45 @@
-cancer <- NormalizeData(cancer, normalization.method = "LogNormalize", scale.factor = 1e5)
+cancer <- FindVariableFeatures(cancer, selection.method = "vst", nfeatures = 300)
 cancer[["percent.mt"]] <- PercentageFeatureSet(cancer, pattern = "^MT-")
 cancer <- subset(cancer, subset= percent.mt<10)
 #
 # merge all the tig data
 #
-cancer <- FindVariableFeatures(cancer, selection.method = "vst", nfeatures = 300)
-cancer <- NormalizeData(cancer,assay="DTD",normalization.method = "CLR",scale.factor = 1e6)
+cancer <- NormalizeData(cancer, normalization.method = "LogNormalize", scale.factor = 1e5)
+cancer <- NormalizeData(cancer,assay="DTD",normalization.method = "CLR",scale.factor = 1e2)
 #
 # PCA and visualize
 #
 all.genes <- rownames(cancer)
 cancer <- ScaleData(cancer, features = all.genes)
-cancer <- RunPCA(cancer, npcs=20, features = VariableFeatures(object = cancer))
-DimPlot(cancer, reduction = "pca",group.by = "run")
+cancer <- RunPCA(cancer, npcs=50, features = VariableFeatures(object = cancer))
 cancer <- JackStraw(cancer, num.replicate = 100)
 cancer <- ScoreJackStraw(cancer, dims = 1:20)
 JackStrawPlot(cancer, dims = 1:20)
 ElbowPlot(cancer)
-cancer <- FindNeighbors(cancer, dims = 1:10)
-cancer <- FindClusters(cancer, resolution = 0.02)
 #
-# Retreiving the results of the preprocessing from the Seurat object
+# subset second run
 #
-cluster = as.numeric(Idents(cancer))
 cancer <- RunUMAP(cancer, dims = 1:10)
-DimPlot(cancer,reduction="pca")+
-  FeaturePlot(cancer,features = c("dtd_P3CE36","dtd_MC7E37","dtd_FLD004"),
-              reduction = "pca",slot="counts",
-              max.cutoff = 100)
-#
-#identification of each cell type
-#
-cancer <- RenameIdents(cancer, `0` = "PC3_NEP", `1` = "PC3_NEP", `2` = "MCF7_CTL")
-#FindAllMarkers(cancer)
-cancer2_mcf7<-subset(cancer,idents = "MCF7_CTL")
+cancer <- RunTSNE(cancer, npcs=50, features = VariableFeatures(object = cancer))
+cancer <- FindNeighbors(cancer, dims = 1:10)
+cancer <- FindClusters(cancer, resolution = 0.1)
+cluster = as.numeric(Idents(cancer))
+FeatureScatter(cancer,feature1 = "nCount_RNA" ,feature2 = "nFeature_RNA")
+DimPlot(cancer,reduction = "umap")+
+  FeaturePlot(cancer, features = c("DUSP1","CAVIN3","CDKN1A","CYBA","S100A2","dtd_FLD004","dtd_P3NE35","dtd_MC7E37", "nFeature_RNA","percent.mt"),reduction = "umap")
+cancer <- RenameIdents(cancer, `0` = "dead", `1` = "PC3", `2` = "MCF7", `3` = "PC3")
+cancer[["celltype"]]<-Idents(cancer)
+
+cancer2_mcf7<-subset(cancer,idents = "MCF7")
 cancer2_mcf7[["celltype"]]<-"MCF7"
 cancer2_mcf7[["NEP"]]<-"0V"
 cancer2_mcf7[["hashtag"]]<-"na"
 cancer2_mcf7[["condition"]]<-"normal"
-cancer2_pc3<-subset(cancer,idents="PC3_NEP")
+
+cancer2_pc3<-subset(cancer,idents="PC3")
 cancer2_pc3[["celltype"]]<-"PC3"
 cancer2_pc3[["NEP"]]<-"40V"
 cancer2_pc3[["hashtag"]]<-"36"
 cancer2_pc3[["condition"]]<-"CytoD"
 cancer2<-merge(cancer2_mcf7,y=cancer2_pc3)
-#cancer2 <- subset(cancer2, subset=percent.mt <10)
-cancer2 <- FindVariableFeatures(cancer2, selection.method = "vst", nfeatures = 100)
-#
-# PCA and visualize
-all.genes <- rownames(cancer2)
-cancer2 <- ScaleData(cancer2, features = all.genes)
-cancer2 <- RunPCA(cancer2, npcs=20, features = VariableFeatures(object = cancer2))
-DimPlot(cancer2, reduction = "pca",group.by = "celltype")
-
-#DimPlot(cancer2, reduction = "umap")+
-  DimPlot(subset(cancer2, subset= percent.mt<5),reduction="pca")+
-    FeaturePlot(subset(cancer2, subset= percent.mt<5),features =c("CYBA","SERPINA3","PLAU","FOSL1"), reduction = "pca")+
-    VlnPlot(subset(cancer2, subset= percent.mt<5),features = "dtd_FLD004", group.by = "celltype")+
-    VlnPlot(subset(cancer2, subset= percent.mt<5),features = "FOSL1", group.by = "celltype")
-FeatureScatter(cancer2,feature1 = "dtd_P3CE36",feature2 = "dtd_FLD004",slot = "counts")
-VlnPlot(cancer2,features =c("percent.mt","nCount_RNA"), group.by = "celltype")
+rm(cancer2_mcf7, cancer2_pc3)
