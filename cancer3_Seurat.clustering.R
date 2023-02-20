5346a4958e4aff2ff2a5f9dec20202590ce8baa2
A-Shiomi <akifumi.shiomi@riken.jp>
2023-02-20 14:08:20 +0900

    230220

--- cancer3_Seurat.clustering.R
+++ cancer3_Seurat.clustering.R
@@ -1,77 +1,52 @@
 cancer <- FindVariableFeatures(cancer, selection.method = "vst", nfeatures = 300)
-cancer <- NormalizeData(cancer,assay="DTD",normalization.method = "CLR",scale.factor = 1e2)
 cancer[["percent.mt"]] <- PercentageFeatureSet(cancer, pattern = "^MT-")
 cancer <- subset(cancer, subset= percent.mt<10)
+#
+# merge all the tig data
+#
+cancer <- NormalizeData(cancer, normalization.method = "LogNormalize", scale.factor = 1e5)
+cancer <- NormalizeData(cancer,assay="DTD",normalization.method = "CLR",scale.factor = 1e2)
 #
 # PCA and visualize
 #
 all.genes <- rownames(cancer)
 cancer <- ScaleData(cancer, features = all.genes)
 cancer <- RunPCA(cancer, npcs=50, features = VariableFeatures(object = cancer))
-
-DimPlot(cancer, reduction = "pca",group.by = "run")+
-  DimPlot(cancer,reduction="pca",group.by = "celltype")+
-  FeaturePlot(cancer,features = "dtd_FLD004")
 cancer <- JackStraw(cancer, num.replicate = 100)
 cancer <- ScoreJackStraw(cancer, dims = 1:20)
 JackStrawPlot(cancer, dims = 1:20)
 ElbowPlot(cancer)
-cancer <- FindNeighbors(cancer, dims = 1:10)
-cancer <- FindClusters(cancer, resolution = 0.05)
-#
-# Retreiving the results of the preprocessing from the Seurat object
-#
-cluster = as.numeric(Idents(cancer))
-cancer <- RunUMAP(cancer, dims = 1:10)
-DimPlot(cancer, reduction = "pca")+
-  DimPlot(subset(cancer,subset=run=="third"),reduction="pca")+
-  FeaturePlot(cancer,features = c("dtd_FLD500"), reduction = "umap")+
-  DimPlot(subset(cancer,subset=run=="first"),reduction="pca")
-
 #
 # subset third run
 #
-cancer3<-subset(cancer,subset=run=="third")
-DimPlot(subset(cancer3,subset=percent.mt<5),reduction = "pca")+
-  FeaturePlot(subset(cancer3,subset=percent.mt<5),
-              features = c("CCDC190","CAVIN3","CD163L1"),reduction = "pca",
-              max.cutoff = 4)
-cancer3 <- FindVariableFeatures(cancer3, selection.method = "vst", nfeatures = 300)
-all.genes <- rownames(cancer3)
-cancer3 <- ScaleData(cancer3, features = all.genes)
-cancer3 <- RunPCA(cancer3, npcs=50, features = VariableFeatures(object = cancer3))
-cancer3 <- RunTSNE(cancer3, npcs=50, features = VariableFeatures(object = cancer3))
-cancer3 <- FindNeighbors(cancer3, dims = 1:10)
-cancer3 <- FindClusters(cancer3, resolution = 0.05)
-DimPlot(cancer3, reduction = "tsne")+
-  FeaturePlot(cancer3,features = "dtd_FLD004", reduction = "tsne")
-DimPlot(cancer3,reduction = "tsne")+
-  FeaturePlot(cancer3, features = c("CYBA","CAVIN3","S100A2","FOSL1", "CDKN1A","DUSP1"),reduction = "tsne", max.cutoff = 4)
-cancer3 <- RenameIdents(cancer3, `0` = "PC3", `1` = "MCF10A", `2` = "MDAMB231")
+cancer <- RunUMAP(cancer, dims = 1:10)
+cancer <- RunTSNE(cancer, npcs=50, features = VariableFeatures(object = cancer))
+cancer <- FindNeighbors(cancer, dims = 1:10)
+cancer <- FindClusters(cancer, resolution = 0.1)
+cluster = as.numeric(Idents(cancer))
+FeatureScatter(cancer,feature1 = "nCount_RNA" ,feature2 = "nFeature_RNA")
+DimPlot(cancer,reduction = "tsne")+
+  FeaturePlot(cancer, features = c("DUSP1","CAVIN3","CYBA","S100A2","dtd_FLD004","dtd_MM2E32", "dtd_P3HE33", "nFeature_RNA","percent.mt"),reduction = "tsne")
+cancer <- RenameIdents(cancer, `0` = "PC3", `1` = "MCF10A", `2` = "MDAMB231", `3` = "PC3", `4` = "MCF10A")
 
-cancer_mcf10<-subset(cancer3,idents = "MCF10A")
+cancer_mcf10<-subset(cancer,idents = "MCF10A")
 cancer_mcf10[["celltype"]]<-"MCF10A"
 cancer_mcf10[["NEP"]]<-"0V"
 cancer_mcf10[["hashtag"]]<-"na"
 cancer_mcf10[["condition"]]<-"normal"
 
-cancer_pc3<-subset(cancer3,idents="PC3")
+cancer_pc3<-subset(cancer,idents="PC3")
 cancer_pc3[["celltype"]]<-"PC3"
 cancer_pc3[["condition"]]<-"normal"
 cancer_pc3[["NEP"]]<-"75V"
 cancer_pc3[["hashtag"]]<-"P3NE33"
 
-cancer_mda<-subset(cancer3,idents="MDAMB231")
+cancer_mda<-subset(cancer,idents="MDAMB231")
 cancer_mda[["celltype"]]<-"MDAMB231"
 cancer_mda[["condition"]]<-"normal"
 cancer_mda[["NEP"]]<-"40V"
 cancer_mda[["hashtag"]]<-"MM2E32"
 
-cancer3_merge<- merge(cancer_mcf10,y=cancer_pc3)
-cancer3_merge<-merge(cancer3_merge,y=cancer_mda)
-cancer_merge123 <-merge(cancer_merge,y=cancer3_merge)
-cancer_merge123 <- FindVariableFeatures(cancer_merge123, selection.method = "vst", nfeatures = 300)
-all.genes <- rownames(cancer_merge123)
-cancer_merge123 <- ScaleData(cancer_merge123, features = all.genes)
-cancer_merge123 <- RunPCA(cancer_merge123, npcs=20, features = VariableFeatures(object = cancer_merge123))
-DimPlot(cancer_merge123,group.by = "celltype")
+cancer3 <- merge(cancer_mcf10,y=cancer_pc3)
+cancer3 <- merge(cancer3,y=cancer_mda)
+rm(cancer, cancer_pc3, cancer_mda, cancer_mcf10)
