c6d3079fc1c1f0e2b772a6b497b6cc8caed0fd1c
A-Shiomi <akifumi.shiomi@riken.jp>
2023-02-20 14:07:48 +0900

    230220

--- cancer6_Seurat.clustering.R
+++ cancer6_Seurat.clustering.R
@@ -1,31 +1,33 @@
 cancer <- FindVariableFeatures(cancer, selection.method = "vst", nfeatures = 300)
-cancer <- NormalizeData(cancer,assay="DTD",normalization.method = "CLR",scale.factor = 1e2)
 cancer[["percent.mt"]] <- PercentageFeatureSet(cancer, pattern = "^MT-")
-cancer <- subset(cancer,subset=percent.mt<10)
+cancer <- subset(cancer, subset= percent.mt<10)
+#
+# merge all the tig data
+#
+cancer <- NormalizeData(cancer, normalization.method = "LogNormalize", scale.factor = 1e5)
+cancer <- NormalizeData(cancer,assay="DTD",normalization.method = "CLR",scale.factor = 1e2)
 #
 # PCA and visualize
 #
 all.genes <- rownames(cancer)
 cancer <- ScaleData(cancer, features = all.genes)
 cancer <- RunPCA(cancer, npcs=50, features = VariableFeatures(object = cancer))
-DimPlot(cancer, reduction = "pca",group.by = "run")+
-  FeaturePlot(cancer,features = "dtd_FLD004")
 cancer <- JackStraw(cancer, num.replicate = 100)
 cancer <- ScoreJackStraw(cancer, dims = 1:20)
 JackStrawPlot(cancer, dims = 1:20)
 ElbowPlot(cancer)
 #
-# subset fifth run
+# subset sixth run
 #
 cancer <- RunUMAP(cancer, dims = 1:10)
 cancer <- RunTSNE(cancer, npcs=50, features = VariableFeatures(object = cancer))
 cancer <- FindNeighbors(cancer, dims = 1:10)
-cancer <- FindClusters(cancer, resolution = 0.04)
+cancer <- FindClusters(cancer, resolution = 0.1)
 cluster = as.numeric(Idents(cancer))
 
-DimPlot(cancer,reduction = "umap")+
-  FeaturePlot(cancer, features = c("DUSP1","CAVIN3","CDKN1A","CYBA","S100A2", "nCount_RNA"),reduction = "umap")
-cancer <- RenameIdents(cancer, `2` = "MCF10A", `1` = "MCF7", `0` = "unknown")
+DimPlot(cancer,reduction = "tsne")+
+  FeaturePlot(cancer, features = c("DUSP1","CAVIN3","CDKN1A","CYBA","S100A2", "dtd_FLD004", "nFeature_RNA","percent.mt"),reduction = "tsne")
+cancer <- RenameIdents(cancer, `0` = "MCF7", `1` = "dead", `2` = "dead", `3` = "MCF10A")
 
 cancer_mcf10<-subset(cancer,idents = "MCF10A")
 cancer_mcf10[["celltype"]]<-"MCF10A"
@@ -39,23 +41,5 @@ cancer_mcf7[["condition"]]<-"normal"
 cancer_mcf7[["NEP"]]<-"40V"
 cancer_mcf7[["hashtag"]]<-"MC7E37"
 
-cancer6_merge<- merge(cancer_mcf10,y=cancer_mcf7)
-
-cancer_merge123456 <-merge(cancer_merge12345,y=cancer6_merge)
-cancer_merge123456 <- subset(cancer_merge123456,subset=nFeature_RNA>2500)
-cancer_merge123456 <- FindVariableFeatures(cancer_merge123456, selection.method = "vst", nfeatures = 300)
-all.genes <- rownames(cancer_merge123456)
-cancer_merge123456 <- ScaleData(cancer_merge123456, features = all.genes)
-cancer_merge123456 <- RunPCA(cancer_merge123456, npcs=20, features = VariableFeatures(object = cancer_merge123456))
-DimPlot(cancer_merge123456,group.by = "celltype")
-cancer_merge123456 <- RunUMAP(cancer_merge123456, dims = 1:10)
-cancer_merge123456 <- RunTSNE(cancer_merge123456,dims=1:10)
-
-  FeaturePlot(cancer_merge123456,
-              features = c("CYBA","S100A2","CAVIN3","FOSL1","CDKN1A","DUSP1"),
-              reduction = "tsne",
-              max.cutoff = 10)+
-    DimPlot(cancer_merge123456,reduction = "tsne",group.by = "celltype")+
-    DimPlot(cancer_merge123456,reduction = "tsne",group.by = "NEP")
-
-rm(cancer,cancer_mcf10,cancer_mcf7,cancer6_merge, cancer6, cancer_merge1234, cancer_merge12345)
+cancer6 <- merge(cancer_mcf10,y=cancer_mcf7)
+rm(cancer,cancer_mcf10,cancer_mcf7)
