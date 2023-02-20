431384716a913afdff42a7eda1b265eef00831e7
A-Shiomi <akifumi.shiomi@riken.jp>
2023-02-20 14:08:05 +0900

    230220

--- cancer5_Seurat.clustering.R
+++ cancer5_Seurat.clustering.R
@@ -1,7 +1,11 @@
 cancer <- FindVariableFeatures(cancer, selection.method = "vst", nfeatures = 300)
-cancer <- NormalizeData(cancer,assay="DTD",normalization.method = "CLR",scale.factor = 1e2)
 cancer[["percent.mt"]] <- PercentageFeatureSet(cancer, pattern = "^MT-")
-cancer <- subset(cancer,subset=percent.mt<10)
+cancer <- subset(cancer, subset= percent.mt<10)
+#
+# merge all the tig data
+#
+cancer <- NormalizeData(cancer, normalization.method = "LogNormalize", scale.factor = 1e5)
+cancer <- NormalizeData(cancer,assay="DTD",normalization.method = "CLR",scale.factor = 1e2)
 #
 # PCA and visualize
 #
@@ -22,9 +26,12 @@ cancer <- FindNeighbors(cancer, dims = 1:10)
 cancer <- FindClusters(cancer, resolution = 0.1)
 cluster = as.numeric(Idents(cancer))
 
-DimPlot(cancer,reduction = "tsne")+
-  FeaturePlot(cancer, features = c("DUSP1","CAVIN3","CDKN1A","CYBA","percent.mt", "S100A2"),reduction = "tsne", max.cutoff = 4)
-cancer <- RenameIdents(cancer, `2` = "unknown", `1` = "MCF10A", `0` = "MCF7")
+DimPlot(cancer,reduction = "umap")+
+  FeaturePlot(cancer, features = c("DUSP1","CAVIN3","CYBA","S100A2","dtd_FLD004", "nFeature_RNA","percent.mt"),reduction = "umap")
+DimPlot(cancer,reduction = "umap")+
+  FeaturePlot(cancer, features = c("dtd_FLD004"),reduction = "umap")
+
+cancer <- RenameIdents(cancer, `0` = "MCF10A", `1` = "MCF7", `2` = "MCF7", `3` = "dead")
 
 cancer_mcf10<-subset(cancer,idents = "MCF10A")
 cancer_mcf10[["celltype"]]<-"MCF10A"
@@ -38,14 +45,6 @@ cancer_mcf7[["condition"]]<-"normal"
 cancer_mcf7[["NEP"]]<-"0V"
 cancer_mcf7[["hashtag"]]<-"na"
 
-cancer5_merge<- merge(cancer_mcf10,y=cancer_mcf7)
-cancer_merge12345 <-merge(cancer_merge1234,y=cancer5_merge)
-cancer_merge12345 <- FindVariableFeatures(cancer_merge12345, selection.method = "vst", nfeatures = 300)
-all.genes <- rownames(cancer_merge12345)
-cancer_merge12345 <- ScaleData(cancer_merge12345, features = all.genes)
-cancer_merge12345 <- RunPCA(cancer_merge12345, npcs=20, features = VariableFeatures(object = cancer_merge1234))
-DimPlot(cancer_merge12345,group.by = "celltype")
-
-
+cancer5<- merge(cancer_mcf10,y=cancer_mcf7)
 rm(cancer,cancer_mcf10,cancer_mcf7)
-rm(cancer5,cancer5_merge)
+
